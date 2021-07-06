7e53d7f9f48b6124c340460dc6e3c11da02c281b
Felipe Borges <felbgs12@gmail.com>
2021-07-06 09:54:34 -0300

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react';
-import { RiSearchLine } from 'react-icons/ri';
+import { Avatar, Box, Input, Icon, HStack, Flex, Text } from '@chakra-ui/react';
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from 'react-icons/ri';
 
 export function Header() {
   return (
@@ -43,6 +47,34 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Box mr="4" textAlign="right">
+          <Text>Felipe Borges</Text>
+          <Text color="gray.300" fontSize="small">
+            felbgs12@gmail.com
+          </Text>
+        </Box>
+
+        <Avatar
+          size="md"
+          name="Felipe Borges"
+          src="https://github.com/felbgs12.png"
+        />
       </Flex>
     </Flex>
   );
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,9 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+
+export default function Dashboard() {
+  return (
+    <Flex h="100%" w="100%">
+      <Text ml="auto" mt="auto">Dashboard</Text>
+    </Flex>
+  );
+}
